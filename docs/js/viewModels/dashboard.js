1cb6ef0a6576e4f2d06a1316c370dfff9edcec43
Donald Raikes <don.raikes@oracle.com>
2019-11-25 18:10:32 +0000

    After release build on master branch.

---
+++ docs/js/viewModels/dashboard.js
@@ -0,0 +1,7 @@
+/**
+ * @license
+ * Copyright (c) 2014, 2019, Oracle and/or its affiliates.
+ * The Universal Permissive License (UPL), Version 1.0
+ * @ignore
+ */
+define(["accUtils"],(function(n){return function(){this.connected=function(){n.announce("Dashboard page loaded.","assertive"),document.title="Dashboard"},this.disconnected=function(){},this.transitionCompleted=function(){}}}));
